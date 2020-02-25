46495b42ee98c61237627dcc35fac627052c1393
nataliiatkachenko <azaria1993@gmail.com>
2020-02-25 10:34:16 +0200

    add Context, connect to DiaryDB (Entity Framework Core)

---
+++ TestProject/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestProject.Models
+{
+    public class Context: DbContext
+    {
+        public DbSet<Classes> Classes { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- TestProject/Startup.cs
+++ TestProject/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TestProject.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestProject
 {
@@ -20,6 +22,10 @@ namespace TestProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection =
+               Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<Context>(options => options.UseSqlServer(connection));
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
