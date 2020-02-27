0f71b87608b4be56c26a23c71d1d894acc81ffc2
nataliiatkachenko <azaria1993@gmail.com>
2020-02-27 14:43:44 +0200

    add Classes, Context, edit HomeController, set connection to DiaryDB

--- TestProject/TestProject/Controllers/HomeController.cs
+++ TestProject/TestProject/Controllers/HomeController.cs
@@ -11,27 +11,15 @@ namespace TestProject.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        DiaryContext db;
+        public HomeController(DiaryContext context)
         {
-            _logger = logger;
+            db = context;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(db.Classes.ToList());
         }
     }
 }
---
+++ TestProject/TestProject/Models/DiaryContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestProject.Models
+{
+    public class DiaryContext: DbContext
+    {
+        public DbSet<Classes> Classes { get; set; }
+
+        public DiaryContext(DbContextOptions<DiaryContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
