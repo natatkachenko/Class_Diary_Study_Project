45f0ee371f0a7db5f6be2cab770eeaee4f6efa43
nataliiatkachenko <azaria1993@gmail.com>
2020-03-03 16:24:30 +0200

    delete constructor and config method in DiaryDBContext, add operation "Add" for Classes

--- TestProject/TestProject/Controllers/HomeController.cs
+++ TestProject/TestProject/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using TestProject.Models;
 
 namespace TestProject.Controllers
@@ -17,9 +18,25 @@ namespace TestProject.Controllers
             db = context;
         }
 
-        public IActionResult Index()
+        // вывод списка классов
+        public async Task<IActionResult> Index()
         {
-            return View(db.Classes.ToList());
+            return View(await db.Classes.ToListAsync());
+        }
+
+        // вызов формы для добавления класса
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // добавление нового класса в бд
+        [HttpPost]
+        public async Task<IActionResult> Add(Classes classes)
+        {
+            db.Classes.Add(classes);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
