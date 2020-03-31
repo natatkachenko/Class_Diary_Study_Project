b7328966eb2fd5b7c7a897426f09b0ebdf774999
nataliiatkachenko <azaria1993@gmail.com>
2020-03-31 16:16:43 +0300

    add functionality of ClassSubject, menu

---
+++ TestProject/TestProject/Controllers/ClassSubjectController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestProject.Models;
+
+namespace TestProject.Controllers
+{
+    public class ClassSubjectController : Controller
+    {
+        DiaryDBContext db;
+        public ClassSubjectController(DiaryDBContext context)
+        {
+            db = context;
+        }
+
+        // вывод списка классов, у которых ведётся выбранный предмет
+        [HttpGet]
+        public IActionResult Index(string name)
+        {
+            if (name != null)
+            {
+                var classes =
+                    db.ClassSubject.FromSqlInterpolated($"Select * From ClassSubject Where SubjectName={name}").ToList();
+                if (classes != null)
+                    return View(classes);
+            }
+            return NotFound();
+        }
+    }
+}
