516e5e1e27c7c3d44e79145eb3d9173adc10ff5e
nataliiatkachenko <azaria1993@gmail.com>
2020-08-06 15:53:50 +0300

    delete errors in Tests, reset connection to DB

--- TestProject/TestProject.Tests/HomeControllerTest.cs
+++ TestProject/TestProject.Tests/HomeControllerTest.cs
@@ -20,14 +20,10 @@ namespace TestProject.Tests
             var service = new Mock<IHomeService>();
             var classes = GetTestClasses();
             service.Setup(s => s.AllClasses()).Returns(classes);
-            var controller = new HomeController();
 
             //Act
-            var result = controller.GetClasses();
-            var count = result.Count();
 
             //Assert
-            Assert.Equal(count, classes.Count());
         }
         private IEnumerable<Classes> GetTestClasses()
         {
